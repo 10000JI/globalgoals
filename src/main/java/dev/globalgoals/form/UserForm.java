80268548e394a1b8d28dbade2bf043493e17bbd0
10000JI <sksk436@naver.com>
2023-11-07 13:19:19 +0900

    User Join Form

--- src/main/java/dev/globalgoals/form/UserForm.java
+++ src/main/java/dev/globalgoals/form/UserForm.java
@@ -21,7 +21,7 @@ public class UserForm {
     @Pattern(regexp = "(?=.*[0-9])(?=.*[a-zA-Z])(?=.*\\W)(?=\\S+$).{8,16}", message = "8~16자 영문 대 소문자, 숫자, 특수문자를 사용하세요.")
     private String password;
 
-    @NotBlank(message = "비밀번호체크: 필수 정보입니다.")
+    @NotBlank(message = "비밀번호 확인: 필수 정보입니다.")
     @Pattern(regexp = "(?=.*[0-9])(?=.*[a-zA-Z])(?=.*\\W)(?=\\S+$).{8,16}", message = "8~16자 영문 대 소문자, 숫자, 특수문자를 사용하세요.")
     private String passwordCheck;
 
